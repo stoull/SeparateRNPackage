bd37bec519659e3ebb59fc92cfb5d776c8463bd6
Hut Chen <changchunstep@126.com>
2024-02-19 09:44:14 +0800

    增加各模块

---
+++ Apps/InverterKit.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const InverterKit = () => {
+    return <Text>Here is InverterKit module</Text>;
+};
+
+export default InverterKit;
---
+++ index-inverterkit.js
@@ -0,0 +1,7 @@
+import { AppRegistry } from 'react-native';
+
+import InverterKit from './Apps/InverterKit';
+import { inverterKit as appName } from './app.json';
+
+
+AppRegistry.registerComponent(appName, InverterKit);
